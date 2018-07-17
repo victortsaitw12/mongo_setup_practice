e4efa78f26a62d309a30548556d18f7aa5656a8a
victor <victor.tsai@astrocorp.com.tw>
2018-07-17 12:37:36 +0800

    fix the ip bugs

--- mongo_sharding/init.sh
+++ mongo_sharding/init.sh
@@ -3,6 +3,6 @@ docker-compose up -d
 
 docker-compose exec mongocfg1 sh -c "mongo --port 27017 < /scripts/init-config.js"
 sleep 20
-docker-compose exec mongos1 sh -c "mongo < /scripts/init-router.js"
+docker-compose exec mongos1 sh -c "mongo --port 27017 < /scripts/init-router.js"
 sleep 20
-docker-compose exec mongos1 sh -c "mongo < /scripts/test_sharding.js"
+docker-compose exec mongos1 sh -c "mongo --port 27017 < /scripts/test_sharding.js"
--- mongo_sharding/scripts/test_sharding.js
+++ mongo_sharding/scripts/test_sharding.js
@@ -1,5 +1,5 @@
 use testDB
 sh.enableSharding("testDB")
-db.createCollection("testDB.testCollection")
+db.createCollection("testCollection")
 sh.shardCollection("testDB.testCollection", {"testField": "hashed"})
 for(var i = 0; i < 10000; i++){ db.testCollection.insert({"testField": i}) }
